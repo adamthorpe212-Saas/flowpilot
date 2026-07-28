109cd5820e21e519e9d187279cffc3c802189646
Adam Thorpe <adamthorpe212@gmail.com>
2026-07-28 22:45:07 +0100

    Initial FlowPilot setup

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-6xl font-bold">FlowPilot</h1>
+    </main>
+  );
+}
